e7cc963ab58c84243222bc84f0c9eae5aba107d9
starseed2021 <tiff.harper18@gmail.com>
2022-01-12 19:40:51 -0500

    created new data file with unique identifiers

--- app/data/unique_id.py
+++ app/data/unique_id.py
@@ -1,7 +1,15 @@
 import json
+import uuid
 
 with open('./data.json') as f:
     data = json.load(f)
-    for location in data[0]['oysters']['regions']:
+    for region, locations in data['oysters']['regions'].items():
+        for location, oysters in locations.items():
+            for oyster, oyster_profile in oysters.items():
+                # Gives a string with unique id, have to set the id to each key in region
+                oyster_id = str(uuid.uuid4())
+                oyster_profile["id"] = oyster_id
+                print(region, location, oyster)
 
-        print(location.keys())
+with open('./new_data.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
